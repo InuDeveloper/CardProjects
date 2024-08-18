0c4e1f256fd780a92e7645be534192c6bd01ac79
ShiibaDev <shiibadev@gmail.com>
2024-08-18 15:27:39 -0400

    We going to pause the development of this app for a while.

--- src/script/button.js
+++ src/script/button.js
@@ -1,4 +1,7 @@
 // Button behaviour
 const CreateCardElement = () => {
     // Function when button is clicked.
+    const Button = document.getElementById('createCard')
+
+    const button = console.log(`Button clicked, func status: ${CreateCardElement}`)
 }
